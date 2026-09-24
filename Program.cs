f251f2f892b470013bc8c394b172ebb5f5fd05ab
NayaraYM <74890597@continental.edu.pe>
2026-09-24 11:35:34 -0500

    Implement CORS and configure dynamic port in Program.cs
    Added CORS support to the application and modified the run command to use an environment variable for the port.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy( policity =>
+            {
+                policity
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+            }
+        )
+    }
+)
 var app = builder.Build();
 
 app.MapGet("/",() =>
@@ -24,4 +36,5 @@ app.MapGet("/api/panaderia",() =>
 });
 
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("Port")??"10000",
+app.Run($"http://0.0.0.0:{port}");
